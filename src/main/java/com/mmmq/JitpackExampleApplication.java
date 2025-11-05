e7b6b55b48b49a0ea1b64e43b7a890d2229691ff
KimDaehyeon <daehyeon3351@gmail.com>
2025-11-05 20:13:23 +0900

    setting: init spring boot app

---
+++ src/main/java/com/mmmq/JitpackExampleApplication.java
@@ -0,0 +1,13 @@
+package com.mmmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JitpackExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JitpackExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mmmq/JitpackExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JitpackExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
